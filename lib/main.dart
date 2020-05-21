0546541ca7b5c50372aa9ae993672b957775d200
Pablo <pilj@virket.com>
2020-05-21 00:03:33 -0500

    finishing char add FittedBox to adjust the size of widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:expense_planner/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
 
 import './widgets/transaction_list.dart';
 import './widgets/new_transaction.dart';
+import './widgets/chart.dart';
 import './models/transaction.dart';
 
 void main() => runApp(MyApp());
@@ -59,6 +59,17 @@ class _MyHomePageState extends State<MyHomePage> {
     //   date: DateTime.now(),
     // ),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTrsansaction(String txTitle, double txAmount) {
     final newTx = Transaction(
       title: txTitle,
@@ -103,14 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
